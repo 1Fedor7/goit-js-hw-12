b08a4ae2d2caa66096d7d154538dc59d815b5fa5
Fedor <fiafedor7@gmail.com>
2024-10-01 22:23:13 +0200

    update files

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,18 +4,13 @@ const API_KEY = '46204158-4df5f152d7953f3c8acc30a03';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1, perPage = 15) {
-    const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
-    try {
-        const response = await axios.get(url);
-
-        if (response.status !== 200 || !response.data || !response.data.hits) {
-            throw new Error('Invalid response format');
-        }
-
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching images:', error);
-        throw error;
-    }
+  try {
+    const { data } = await axios.get(url);
+    return data.hits;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error;
+  }
 }
