cbee6df8bdf8f7bcd2e4ba6aa8d8bb84736435b9
Fedor <fiafedor7@gmail.com>
2024-10-01 11:07:28 +0200

    Update render-functions.js

---
+++ src/js/render-functions.js
@@ -0,0 +1,41 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+let lightbox;
+
+export function clearGallery() {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = '';
+}
+
+export function renderImages(images) {
+  const gallery = document.querySelector('.gallery');
+  
+  const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+    <a class="gallery__item" href="${largeImageURL}">
+      <img class="gallery__image" src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item"><b>Likes:</b> ${likes}</p>
+        <p class="info-item"><b>Views:</b> ${views}</p>
+        <p class="info-item"><b>Comments:</b> ${comments}</p>
+        <p class="info-item"><b>Downloads:</b> ${downloads}</p>
+      </div>
+    </a>`).join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  if (!lightbox) {
+    lightbox = new SimpleLightbox('.gallery a');
+  } else {
+    lightbox.refresh();
+  }
+}
+
+export function showNotification(message) {
+  iziToast.error({
+    title: 'Error',
+    message: message,
+  });
+}
