a79858d335dd687fbbb4b03e7331c49b16e5f9e4
Fedor <fiafedor7@gmail.com>
2024-10-05 17:38:54 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -5,12 +5,12 @@ const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1, perPage = 15) {
   const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
-
+  
   try {
-    const { data } = await axios.get(url);
-    return data.hits;
+    const response = await axios.get(url);
+    return response.data.hits;
   } catch (error) {
-    console.error('Error fetching data:', error);
+    console.error('Error:', error);
     throw error;
   }
 }
