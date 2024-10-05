5a3dfa9df10ebac240c9a60012c03d55a7fa7dff
Fedor <fiafedor7@gmail.com>
2024-10-05 18:02:26 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
-
 const API_KEY = '46204158-4df5f152d7953f3c8acc30a03';
 const BASE_URL = 'https://pixabay.com/api/';
+const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 
 export async function fetchImages(query, page = 1, perPage = 15) {
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+  const url = `${PROXY_URL}${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
   
   try {
     const response = await axios.get(url);
